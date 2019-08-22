c29527f953c50a118dd7b9243f53c0c941553bed
GILLIANMCD <gillian2677@gmail.com>
2019-08-22 11:48:33 +0000

    test spp.py added

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipe.html", recipe=mongo.db.recipe.find())
+    return render_template("recipe.html", recipes=mongo.db.recipe.find())
 
 @app.route('/add_recipe')
 def add_recipe():
@@ -28,9 +28,11 @@ def insert_recipe():
     recipe.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
 
-@app.route('/get_carousel')
-def get_carousel():
-    return render_template("carousel.html", carousel=mongo.db.utensils.find())
+@app.route('/add_carousel')
+def add_carousel():
+    return render_template("carousel.html",
+    utensils=mongo.db.utensils.find())
+
 
 
 if __name__ == '__main__':
